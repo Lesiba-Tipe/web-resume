b8b5f86d77fb86beb270d19e3d26cbe16ef5ed77
Lesiba Moshweu <moshweutipe@gmail.com>
2023-01-02 12:59:46 +0200

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.sendFile('./pages/index.html',{root:__dirname}))
+  //.get('/', (req, res) => res.sendFile('./pages/index.html',{root:__dirname}))
+  .get('/', (req, res) => res.send('Server laoded successfully'))
   .get('/data', (req, res) => res.sendFile('./data.json', {root:__dirname}))
   .listen(PORT, () => console.log(`Listening on localhost:${ PORT }`))
